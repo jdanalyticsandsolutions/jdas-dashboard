0b781a1a7e3109a1e7e2c99851376f852ecef4d0
jdanalyticsandsolutions <jasondrunasky@jdanalyticsandsolutions.com>
2025-10-12 21:55:28 -0500

    Update app.py

--- app.py
+++ app.py
@@ -228,7 +228,7 @@ def build_select(entity_set: str, columns: List[str], orderby: Optional[str] = N
 # TABLE CONFIG
 # -------------------------
 TABLES: List[Dict[str, Any]] 
-    {"name": "Trade Deficit Annual", "logical": "cred8_tradedeficitannual","entity_set": "cred8_tradedeficitannuals","path": "/api/trade-deficit-annual", "columns": ["jdas_month", "cred8_chatgpt"],"map_to": ["Month", "Total Deficit"],"orderby": "jdas_month desc"},
+    {"name": "Trade Deficit Annual","logical": "cred8_tradedeficitannual","entity_set": "cred8_tradedeficitannuals","path": "/api/trade-deficit-annual","columns": ["jdas_month", "cred8_chatgpt"],"map_to": ["Month", "Total Deficit"],"orderby": "jdas_month desc"},
     {"name": "Tariff % by Country","logical": "cred8_tariffbycountry","entity_set": "cred8_tariffbycountries","path": "/api/tariff-by-country","columns": [],"map_to": [],"orderby": ""},
     {"name": "Tariff By Item", "logical": "jdas_tariffschedule", "entity_set": "jdas_tariffschedules", "path": "/api/tariff-by-item", "columns": [],"map_to": [], "orderby": "" },
     {"name": "Trade Deals",          "logical": "cred8_tradedeal",          "entity_set": "cred8_tradedeals",          "path": "/api/trade-deals",         "columns": [], "map_to": [], "orderby": ""},
